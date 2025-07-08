fdcc622bce9b666293e7949521805c642b85b8e3
K V L N Kushal <kls2edmentre@gmail.com>
2025-07-09 00:24:05 +0530

    day-1 of rust prog

---
+++ day1.rs
@@ -0,0 +1,16 @@
+fn main(){
+    // day 1 single line comment
+    /* Multi line comment */
+    println!("kore wa watashino hitotsu no command desu");
+    // here the ! is called a macro
+    
+    /*  Think of macros like templates or shortcuts. Instead of repeating code or writing generic logic by hand, you can define a macro once and use it many times.
+
+        There are two types of macros in Rust:
+            Declarative macros: macro_rules! — rule-based
+            Procedural macros: More advanced; they take Rust code as input and output Rust code (like plugins) 
+    */
+    // generally, functions take params but macros consume code so technically it's a code that writes code
+    // macros are invoked with a ! at the end, like println!
+    
+    }
