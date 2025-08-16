d36a6bd8aefd865d940fff422843acbeae0a0af7
K V L N Kushal <kls2edmentre@gmail.com>
2025-08-17 02:06:59 +0530

    added modular functions to control flow type

---
+++ chap_3/target/debug/chap_3.d
@@ -0,0 +1,1 @@
+/home/kvlnkls2kde/Documents/Rust/Everust/chap_3/target/debug/chap_3: /home/kvlnkls2kde/Documents/Rust/Everust/chap_3/src/main.rs
---
+++ chap_3/target/debug/deps/chap_3-d6d5a3b97579ee3b.d
@@ -0,0 +1,5 @@
+/home/kvlnkls2kde/Documents/Rust/Everust/chap_3/target/debug/deps/chap_3-d6d5a3b97579ee3b.d: src/main.rs
+
+/home/kvlnkls2kde/Documents/Rust/Everust/chap_3/target/debug/deps/chap_3-d6d5a3b97579ee3b: src/main.rs
+
+src/main.rs:
